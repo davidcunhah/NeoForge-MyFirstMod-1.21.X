df1f0817bf5cc91ee352a3a25b9a84127bc7250f
davidcunhah <henriqueedavid1@gmail.com>
2026-01-12 15:16:28 -0300

    add smelting

--- src/main/java/me/froggyspk/myfirstmod/item/ModItems.java
+++ src/main/java/me/froggyspk/myfirstmod/item/ModItems.java
@@ -2,6 +2,7 @@ package me.froggyspk.myfirstmod.item;
 
 import me.froggyspk.myfirstmod.MyFirstMod;
 import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
@@ -10,6 +11,8 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.Optional;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MyFirstMod.MOD_ID);
@@ -21,9 +24,14 @@ public class ModItems {
     public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",
             () -> new Item(new Item.Properties()));
 
+
     public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
             () -> new Item(new Item.Properties()));
 
     public static final DeferredItem<Item> AMONG_US = ITEMS.register("among_us",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+                    .nutrition(10)
+                    .saturationModifier(0.5f)
+                    .alwaysEdible()
+                    .build())));
 }
